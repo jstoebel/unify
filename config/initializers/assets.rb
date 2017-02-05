88e6a0acf91e89c7ecfae6fcc7bb3e05c1a53353
Jacob Stoebel <jstoebel@gmail.com>
2017-02-05 07:40:38 -0600

    permiting new stylesheet

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,13 +9,11 @@ Rails.application.config.assets.version = (ENV["ASSETS_VERSION"] || "1.0")
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 
-assets = [
-
-]
-
 Rails.application.config.assets.precompile += %W( draw_map.js
   fb_sdk.js
   home.css
   places.css
   main.css
-  devise/registrations.css)
+  devise/registrations.css
+  devise/sessions.css
+)
